9f271155db407c722d129026a1642c31d370c7cc
Anurag <anuragab28@gmail.com>
2024-07-10 02:10:22 +0530

    first commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MarketingApp from './conponents/MarketingApp';
+
+//console.log(mount);
+
+export default () => {
+    return <MarketingApp />;
+};
---
+++ packages/marketing/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { StylesProvider } from '@material-ui/core/styles';
+
+import Landing from './components/Landing';
+import Pricing from './components/Pricing';
+import ErrorBoundary from './ErrorBoundary';
+
+export default function App() {
+    return (
+        <div>
+            <ErrorBoundary>
+                <StylesProvider>
+                    <BrowserRouter>
+                        <Switch>
+                            <Route exact path="/pricing" component={Pricing} />
+                            <Route path="/" component={Landing} />
+                        </Switch>
+                    </BrowserRouter>
+                </StylesProvider>
+                {/* <h1>Marketing component</h1> */}
+            </ErrorBoundary>
+        </div>
+    );
+};
