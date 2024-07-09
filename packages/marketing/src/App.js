d46f46f222fc62b11fef637146ab599ffd4ac0c8
Anurag <anuragab28@gmail.com>
2024-07-10 03:59:30 +0530

    pipeline

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -18,7 +18,7 @@ export default function App() {
                         </Switch>
                     </BrowserRouter>
                 </StylesProvider>
-                <h1>Marketing component</h1>
+                {/* <h1>Marketing component</h1> */}
             </ErrorBoundary>
         </div>
     );
