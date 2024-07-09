b5b932902800f6596c289a5c479241253ef4dd03
Anurag <anuragab28@gmail.com>
2024-07-10 03:25:10 +0530

    automation

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,7 +1,9 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+//const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const devConfig = {
     mode: 'development',
@@ -17,11 +19,9 @@ const devConfig = {
             remotes: {
                 marketing: 'marketing@http://localhost:8081/remoteEntry.js',
             },
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
+            shared: packageJson.dependencies,
         }),
     ],
 };
 
-module.exports = merge(commonConfig, devConfig);
+module.exports = merge(commonConfig, devConfig)
--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,14 +1,15 @@
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const commonConfig = require('./webpack.common');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const devConfig = {
     mode: 'development',
     devServer: {
         port: 8081,
         historyApiFallback: {
-            index: '/index.html',
+            index: 'index.html',
         },
     },
     plugins: [
@@ -18,7 +19,7 @@ const devConfig = {
             exposes: {
                 './MarketingApp': './src/bootstrap',
             },
-            shared: require('./package.json').dependencies,
+            shared: packageJson.dependencies,
         }),
         new HtmlWebpackPlugin({
             template: './public/index.html',
